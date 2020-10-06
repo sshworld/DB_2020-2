52b50756e4bc6e5ee5f32e3c41ad6972e9d0ce46
sshworld <ssh97540@gmail.com>
2020-10-06 11:28:38 +0900

    주소불러오기

--- routes/myPage/myPageController.js
+++ routes/myPage/myPageController.js
@@ -3,14 +3,54 @@
  *
  */
 
-const db = require('../../config/dbConfig');
+const db = require("../../config/dbConfig");
 
 class myPageController {
+  async myInfo(req, res, next) {
+    const userId = req.params["id"];
 
+    let myAddr = await db("SELECT * FROM ADDR_INFO WHERE USER_ID = ?", [userId]);
 
-    async createCard(req, res, next) {
+    req.body.myAddr = myAddr
 
+    next()
+  }
+
+  async createCard(req, res, next) {
+    next();
+  }
+
+  async createAddr(req, res, next) {
+    try {
+
+        const userId = req.params["id"];
+        const SHIPPING_NUM = req.body.SHIPPING_NUM;
+        const BASIC_ADDR = req.body.BASIC_ADDR;
+        const DETAIL_ADDR = req.body.DETAIL_ADDR;
+
+        if (SHIPPING_NUM == '' || BASIC_ADDR == '' || DETAIL_ADDR == '') {
+            res.send(
+                `<script type="text/javascript">
+                alert("정보를 똑바로 입력하세요"); 
+                location.href='/myPage/:` +
+                userId +
+                `/addAddr';</script>`
+            );
+        } else {
+            await db("INSERT INTO ADDR_INFO SET ?", {
+                USER_ID: userId,
+                SHIPPING_NUM: SHIPPING_NUM,
+                BASIC_ADDR: BASIC_ADDR,
+                DETAIL_ADDR: DETAIL_ADDR
+            });
+
+            next();
+        }
+
+    } catch (error) {
+        console.log(error)
     }
+  }
 }
 
 module.exports = myPageController;
--- routes/myPage/myPageRoute.js
+++ routes/myPage/myPageRoute.js
@@ -8,11 +8,55 @@ const router = express.Router();
 const myPageController = require("./myPageController");
 const myPage = new myPageController();
 
-router.get('/:id', (req, res) => {
-    var sess = req.session.USER_ID;
-    var userName = req.session.USER_NAME;
-    res.render('index.ejs', {pages: './myPage/myPage.ejs', sess : sess, userName : userName});
+router.get("/:id", myPage.myInfo, (req, res) => {
+  var sess = req.session.USER_ID;
+  var userName = req.session.USER_NAME;
+  var myAddr = req.body.myAddr
+  res.render("index.ejs", {
+    pages: "./myPage/myPage.ejs",
+    sess: sess,
+    userName: userName,
+    myAddr:myAddr
+  });
 });
 
+router.get("/:id/addr", (req, res) => {
+  var sess = req.session.USER_ID;
+  var userName = req.session.USER_NAME;
+  res.render("index.ejs", {
+    pages: "./myPage/addAddr.ejs",
+    sess: sess,
+    userName: userName,
+  });
+});
+
+router.post("/:id/addAddr", myPage.createAddr, (req, res) => {
+  var sess = req.session.USER_ID;
+  res.send(
+    `<script type="text/javascript"> alert("주소등록 성공"); location.href='/myPage/:` +
+      sess +
+      `';</script>`
+  );
+});
+
+router.get("/:id/card", (req, res) => {
+  var sess = req.session.USER_ID;
+  var userName = req.session.USER_NAME;
+  res.render("index.ejs", {
+    pages: "./myPage/addCard.ejs",
+    sess: sess,
+    userName: userName,
+  });
+});
+
+router.post("/:id/addCard", myPage.createCard, (req, res) => {
+  var sess = req.session.USER_ID;
+  var userName = req.session.USER_NAME;
+  res.render("index.ejs", {
+    pages: "./myPage/myPage.ejs",
+    sess: sess,
+    userName: userName,
+  });
+});
 
 module.exports = router;
