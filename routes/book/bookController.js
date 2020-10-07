aaf8e0421c9a3fe67b681dabe0dc864e814b4fe9
sshworld <ssh97540@gmail.com>
2020-10-07 23:15:47 +0900

    책 CRUD CLEAR

--- routes/book/bookController.js
+++ routes/book/bookController.js
@@ -4,17 +4,22 @@
  */
 
 const db = require("../../config/dbConfig");
-var multer = require("multer"); //multer 모듈 import
-var upload = multer({ dest: "public/images/book/" }); //업로드 경로 설정
 
 class bookController {
   // 책 정보
   async bookInfo(req, res, next) {
-    let bookInfo = await db("SELECT * FROM book");
 
-    req.body.bookInfo = bookInfo;
+    try {
 
-    next();
+      let bookInfo = await db("SELECT * FROM BOOK");
+
+      req.body.bookInfo = bookInfo
+
+      next();
+
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   // 책 추가
@@ -22,13 +27,16 @@ class bookController {
 
   async addBook(req, res, next) {
     try {
+
       const BOOK_NAME = req.body.BOOK_NAME;
+      const BOOK_PATH = req.file.filename;
       const BOOK_PUBLISH = req.body.BOOK_PUBLISH;
       const BOOK_AUTHOR = req.body.BOOK_AUTHOR;
       const BOOK_DETAIL = req.body.BOOK_DETAIL;
       const BOOK_PRICE = req.body.BOOK_PRICE;
       const BOOK_COUNT = req.body.BOOK_COUNT;
-      const BOOK_FILE = req.body.BOOK_FILE;
+
+
 
       if (
         BOOK_NAME == "" ||
@@ -44,33 +52,15 @@ class bookController {
                 location.href='/book';</script>`
         );
       } else {
-        await db("INSERT INTO ADDR_INFO SET ?", {
-          USER_ID: userId,
-          SHIPPING_NUM: SHIPPING_NUM,
-          BASIC_ADDR: BASIC_ADDR,
-          DETAIL_ADDR: DETAIL_ADDR,
+        await db("INSERT INTO BOOK SET ?", {
+          BOOK_NAME: BOOK_NAME,
+          BOOK_PATH: BOOK_PATH,
+          BOOK_PUBLISH: BOOK_PUBLISH,
+          BOOK_AUTHOR: BOOK_AUTHOR,
+          BOOK_DETAIL: BOOK_DETAIL,
+          BOOK_PRICE: BOOK_PRICE,
+          BOOK_COUNT: BOOK_COUNT
         });
-
-
-        //multer 의 diskStorage를 정의
-        var storage = multer.diskStorage({
-          //경로 설정
-          destination: function (req, file, cb) {
-            cb(null, "publics/images/book/");
-          },
-
-          //실제 저장되는 파일명 설정
-          filename: function (req, file, cb) {
-
-            //Multer는 어떠한 파일 확장자도 추가하지 않습니다.
-            //사용자 함수는 파일 확장자를 온전히 포함한 파일명을 반환해야 합니다.
-            var mimeType = "jpg";
-
-            cb(null, UID + "." + mimeType);
-          },
-        });
-
-        var upload = multer({ storage: storage });
 
         next();
       }
@@ -78,6 +68,55 @@ class bookController {
       console.log(error);
     }
   }
+
+  async updateBook(req, res, next) {
+    try {
+
+      const BOOK_UID = req.params["uid"];
+      const BOOK_NAME = req.body.BOOK_NAME;
+      const BOOK_PATH = req.file.filename;
+      const BOOK_PUBLISH = req.body.BOOK_PUBLISH;
+      const BOOK_AUTHOR = req.body.BOOK_AUTHOR;
+      const BOOK_DETAIL = req.body.BOOK_DETAIL;
+      const BOOK_PRICE = req.body.BOOK_PRICE;
+      const BOOK_COUNT = req.body.BOOK_COUNT;
+
+      if (BOOK_NAME == "" ||
+        BOOK_PUBLISH == "" ||
+        BOOK_AUTHOR == "" ||
+        BOOK_DETAIL == "" ||
+        BOOK_PRICE == "" ||
+        BOOK_COUNT == "") {
+        res.send(
+          `<script type="text/javascript">
+                alert("정보를 똑바로 입력하세요"); 
+                location.href='/book';</script>`
+        );
+      } else {
+        await db("UPDATE BOOK SET BOOK_NAME = ?, BOOK_PATH = ?, BOOK_PUBLISH = ?, BOOK_AUTHOR = ?, BOOK_DETAIL = ?, BOOK_PRICE = ?, BOOK_COUNT = ?  WHERE BOOK_UID = ?", [BOOK_NAME, BOOK_PATH, BOOK_PUBLISH, BOOK_AUTHOR, BOOK_DETAIL, BOOK_PRICE, BOOK_COUNT, BOOK_UID]);
+
+        next();
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteBook(req, res, next) {
+    try {
+
+      const BOOK_UID = req.params["uid"];
+
+      await db("DELETE FROM BOOK WHERE BOOK_UID = ?", [BOOK_UID]);
+      next();
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
 }
 
 module.exports = bookController;
--- routes/book/bookRoute.js
+++ routes/book/bookRoute.js
@@ -7,21 +7,73 @@ const express = require("express");
 const router = express.Router();
 const bookController = require("./bookController");
 const book = new bookController();
+const multer = require("multer"); //multer 모듈 import
+//multer 의 diskStorage를 정의
+const storage = multer.diskStorage({
+    //경로 설정
+    destination: function (req, file, cb) {
+      cb(null, "public/images/book/");
+    },
 
+    //실제 저장되는 파일명 설정
+    filename: function (req, file, cb) {
+
+      //Multer는 어떠한 파일 확장자도 추가하지 않습니다.
+      //사용자 함수는 파일 확장자를 온전히 포함한 파일명을 반환해야 합니다.
+      var mimeType = "jpg";
+
+      const now = Date.now()
+
+      cb(null, now + "." + mimeType);
+    },
+  });
+const upload = multer({
+    storage: storage
+  });
+
+//---------------------------------------라우터-----------------------------------------
+
+// 책 리스트
 router.get('/', book.bookInfo, (req, res) => {
     var sess = req.session.USER_ID;
     const bookInfo = req.body.bookInfo
-    res.render('index.ejs', {pages: './book/bookList.ejs', sess : sess, book:bookInfo});
+    res.render('index.ejs', {pages: './book/bookList.ejs', sess : sess, bookInfo:bookInfo});
 });
 
+// 책 추가
 router.get('/addBook', (req, res) => {
     var sess = req.session.USER_ID;
     res.render('index.ejs', {pages: './book/addBook.ejs', sess : sess});
 });
 
-router.post('/additionBook', book.addBook, (req, res) => {
-    res.send(`<script type="text/javascript"> alert("책 등록 성공"); location.href='/book/';</script>`);
+router.post('/additionBook', upload.single('BOOK_FILE'), book.addBook, (req, res) => {
+    res.send(`<script type="text/javascript"> alert("책 등록 성공"); location.href='/book';</script>`);
 });
+
+// 책 수정
+router.get("/:uid/book", (req, res) => {
+  var sess = req.session.USER_ID;
+  var userName = req.session.USER_NAME;
+  const uid = req.params["uid"];
+  
+  res.render("index.ejs", {
+    pages: "./book/updateBook.ejs",
+    sess: sess,
+    userName: userName,
+    uid: uid
+  });
+})
+
+router.post("/:uid/updateBook", upload.single('BOOK_FILE'), book.updateBook, (req, res) => {
+  res.send(
+    `<script type="text/javascript"> alert("책 수정 성공"); location.href='/book';</script>`
+  );
+});
+
+// 책 삭제
+router.get('/:uid/deleteBook', book.deleteBook, (req, res) => {
+  res.send(`<script type="text/javascript"> alert("책 삭제 성공"); location.href='/book';</script>`);
+})
 
 
 module.exports = router;
